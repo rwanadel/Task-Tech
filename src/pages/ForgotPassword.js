7d3069ea4d16d6f8265fba317a123daeb729c6cb
saraayman <saraaayman855@gmail.com>
2023-05-04 13:00:36 -0700

    adding verify

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import { useNavigate } from "react-router-dom";
+import FormHelperText from "@mui/material/FormHelperText";
+
 
 import logo from "../assets/logo.png";
 import Fpassword from "../assets/ForgotPassword.png";
 import "../styles/ForgotPassword.css";
-import { Link } from "react-router-dom";
+import { isValidEmail } from "../Api/helpers";
+import { insertData } from "../Hook/useInsertData";
+import LoaderComponents from "../components/LoaderComponents";
+import { CustomSnackbar } from "../components/customSnackbar";
+
+
 // import { isValidEmail } from '../Api/helpers';
 const ForgotPassword = () => {
+  const [ showErrors, setShowErrors ] = useState(false);
+  const [ email, setEmail ] = useState('');
+  const [ loading, setLoading ] = useState(false);
+  const [ errorMessage, setErrorMessage ] = useState('')
+
+  const navigate = useNavigate();
+
+  const handleForgetPassword = async () => {
+    try {
+      setLoading(true)
+      const response = await insertData('users/forgetpassword', {email})
+      navigate('/verify')
+    } catch (e) {
+      setLoading(false)
+      setErrorMessage(e?.response?.data?.message ?? 'Error, please try again later')
+    }
+  }
+  
+  const onBtnClick = () => {
+    if(isValidEmail(email)){
+      //call back-end
+      // navigate('/verify')
+      handleForgetPassword()
+    } else {
+      setShowErrors(true)
+    }
+  }
+
   return (
     <Container>
+      <LoaderComponents open={loading} />
+      <CustomSnackbar
+        message={errorMessage}
+        handleClose={() => {
+          setErrorMessage('')
+        }}
+      />
       <Row>
         <Col sm="12">
           <div className="logo-signUp">
@@ -37,16 +81,37 @@ const ForgotPassword = () => {
             E-mail, please check it .
           </div>
           <div
-            style={{ width: "20rem", marginLeft: "1rem", marginTop: "5rem" }}
+            style={{ width: "25rem", marginLeft: "1rem", marginTop: "5rem" }}
           >
-            <TextField
+            {/* <TextField
               style={{ width: "25rem" }}
               id="outlined-helperText"
               label="E-mail"
-            />
-            <Link to={"/verify"}>
-              <button className="btn1">Send Code </button>
-            </Link>
+            /> */}
+            <FormControl
+                sx={{ m: 1 }}
+                fullWidth
+                variant="outlined"
+                error={showErrors && !isValidEmail(email)}
+              >
+                <TextField
+                  error={showErrors && !isValidEmail(email)}
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }}
+                  id="outlined-error"
+                  label="E-mail"
+                  variant="outlined"
+                  fullWidth
+                />
+                <FormHelperText>
+                  {showErrors &&
+                    !isValidEmail(email) &&
+                    "This email is unvalid"}
+                </FormHelperText>
+              </FormControl>
+              <button onClick={onBtnClick} className="btn1">Send Code </button>
           </div>
         </Col>
       </Row>
--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -1,54 +1,163 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
-import logo from "../assets/logo.png"
-import verify from "../assets/Authentication.png"
-import "../styles/Verify.css"
+import logo from "../assets/logo.png";
+import verify from "../assets/Authentication.png";
+import "../styles/Verify.css";
+import { CustomSnackbar } from "../components/customSnackbar";
+import { insertData } from "../Hook/useInsertData";
+import LoaderComponents from "../components/LoaderComponents";
 const Verify = () => {
-    return (
-        <Container>
-            <Row>
-                <Col sm="12" className='logo-verify'>
-                    <img src={logo} alt='logo' />
-                </Col>
-            </Row>
-            <Row>
-                <Col sm="6">
-                    <img src={verify} alt='verify' style={{width:'30rem'}} />
-                </Col>
-                <Col sm="6">
-                    <div className='text1'>
-                        <h3>Verification Code</h3>
-                    </div>
-                    <div className="text2">
-                        Enter the verification code we just sent you on <br/> your email adress .
-                    </div >
-                    <div>
-                        <input type='text' maxLength={1} className='input-verity' />
-                        <input type='text' maxLength={1} className='input-verity' />
-                        <input type='text' maxLength={1} className='input-verity' />
-                        <input type='text' maxLength={1} className='input-verity' />
-                    </div>
 
-                    <div>
-                        <button className='btn1-Verity' >Verity</button>
-                    </div>
-                    <div>
-                        <label className='trans-Verify'>
-                            Don’t recieve a code  ?
-                            <Link to="/Reset" style={{ textDecoration: "none", marginLeft: "0.5rem", fontWeight: 'bold' }}>
-                                <span style={{ cursor: 'pointer', color: ' rgb(22, 80, 105)', fontSize: '18v px' }}>
-                                    Resend
-                                </span>
-                            </Link>
-                        </label>
-                    </div>
-                </Col>
+  const navigate = useNavigate();
 
-            </Row>
-        </Container>
-    )
-}
+  const [values, setValues] = useState({
+    input1: "",
+    input2: "",
+    input3: "",
+    input4: "",
+  });
 
-export default Verify
+  const [isloading, setIsLoading] = useState(false);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (key, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [key]: value,
+    }));
+  };
+
+  const halndleVerify = async () => {
+    try {
+      const { input1, input2, input3, input4 } = values;
+      setIsLoading(true);
+      const response = await insertData("users/verifyresetcode", {
+        resetCode: `${input1}${input2}${input3}${input4}`,
+      });
+      setIsLoading(false);
+      navigate('/reset')
+    } catch (e) {
+      setIsLoading(false);
+      setErrorMessage(
+        e?.response?.data?.message ?? "Error, Please try again later"
+      );
+    }
+  };
+
+  const handleSubmit = () => {
+    if (
+      values.input1.length > 0 &&
+      values.input2.length > 0 &&
+      values.input3.length > 0 &&
+      values.input4.length > 0
+    ) {
+      // talk to back end
+      halndleVerify();
+    } else {
+      setErrorMessage("All Fields are required");
+    }
+  };
+
+  return (
+    <Container>
+      <CustomSnackbar
+        message={errorMessage}
+        handleClose={() => {
+          setErrorMessage("");
+        }}
+      />
+      <LoaderComponents open={isloading} />
+      <Row>
+        <Col sm="12" className="logo-verify">
+          <img src={logo} alt="logo" />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm="6">
+          <img src={verify} alt="verify" style={{ width: "30rem" }} />
+        </Col>
+        <Col sm="6">
+          <div className="text1">
+            <h3>Verification Code</h3>
+          </div>
+          <div className="text2">
+            Enter the verification code we just sent you on <br /> your email
+            adress .
+          </div>
+          <div>
+            <input
+              type="text"
+              maxLength={1}
+              value={values.input1}
+              onChange={(e) => {
+                handleChange("input1", e.target.value);
+              }}
+              className="input-verity"
+            />
+            <input
+              type="text"
+              value={values.input2}
+              onChange={(e) => {
+                handleChange("input2", e.target.value);
+              }}
+              maxLength={1}
+              className="input-verity"
+            />
+            <input
+              type="text"
+              value={values.input3}
+              onChange={(e) => {
+                handleChange("input3", e.target.value);
+              }}
+              maxLength={1}
+              className="input-verity"
+            />
+            <input
+              type="text"
+              value={values.input4}
+              onChange={(e) => {
+                handleChange("input4", e.target.value);
+              }}
+              maxLength={1}
+              className="input-verity"
+            />
+          </div>
+
+          <div>
+            <button className="btn1-Verity" onClick={handleSubmit}>
+              Verity
+            </button>
+          </div>
+          <div>
+            <label className="trans-Verify">
+              Don’t recieve a code ?
+              <Link
+                to="/Reset"
+                style={{
+                  textDecoration: "none",
+                  marginLeft: "0.5rem",
+                  fontWeight: "bold",
+                }}
+              >
+                <span
+                  style={{
+                    cursor: "pointer",
+                    color: " rgb(22, 80, 105)",
+                    fontSize: "18v px",
+                  }}
+                >
+                  Resend
+                </span>
+              </Link>
+            </label>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Verify;
