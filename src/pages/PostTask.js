6b2f3f5b79ed1e40a87e265d4d83bdc6c47e2af3
saraayman <saraaayman855@gmail.com>
2023-05-06 12:06:45 -0700

    adjusting protected, unprotected routes, fixing layout styff

--- src/pages/PostTask.js
+++ src/pages/PostTask.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-import Navbar from "../components/Navbar";
 import "../styles/PostTask.css";
 export const PostTask = () => {
   return (
     <Container>
-      <Navbar/>
       <Row>
         <Col sm="3"></Col>
         <Col sm="6">
           <Form>
             <div>
               <label
-                for="fname"
+                htmlFor="fname"
                 style={{
                   marginTop: "20px",
                   marginLeft: "9px",
@@ -30,14 +32,14 @@ export const PostTask = () => {
                   variant="outlined"
                   fullWidth
                   placeholder="Create responsive design"
-                  style={{background: "#F5F5F5"}}
+                  style={{ background: "#F5F5F5" }}
                 />
               </FormControl>
             </div>
 
             <div>
               <label
-                for="Description"
+                htmlFor="Description"
                 style={{
                   marginTop: "20px",
                   marginLeft: "9px",
@@ -47,36 +49,60 @@ export const PostTask = () => {
                 Tell us about your task
               </label>
               <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
-                <TextField 
+                <TextField
                   id="outlined-multiline-static"
                   placeholder="Description"
                   multiline
                   rows={4}
-                  style={{background: "#F5F5F5"}}
+                  style={{ background: "#F5F5F5" }}
+                />
+              </FormControl>
+            </div>
+            <div>
+              <label
+                htmlFor="Delivery Date"
+                style={{
+                  marginTop: "20px",
+                  marginLeft: "9px",
+                  fontSize: "20px",
+                }}
+              >
+                Delivery Date
+              </label>
+              <FormControl fullWidth sx={{ m: 1 }} variant="outlined">
+                <LocalizationProvider dateAdapter={AdapterMoment}>
+                  <DemoContainer components={["DatePicker"]}>
+                    <DatePicker  label="Basic date picker" />
+                  </DemoContainer>
+                </LocalizationProvider>
+              </FormControl>
+            </div>
+
+            <div>
+              <label
+                htmlFor="salary"
+                style={{
+                  marginTop: "20px",
+                  marginLeft: "9px",
+                  fontSize: "20px",
+                }}
+              >
+                Salary
+              </label>
+              <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
+                <TextField
+                  id="outlined-error"
+                  variant="outlined"
+                  type="number"
+                  fullWidth
+                  style={{ background: "#F5F5F5" }}
                 />
               </FormControl>
             </div>
 
             <div>
               <label
-                for="salary"
-                style={{
-                  marginTop: "20px",
-                  marginLeft: "9px",
-                  fontSize: "20px",
-                  
-                }}
-              >
-                Salary
-              </label>
-              <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
-                <TextField id="outlined-error" variant="outlined" type="number" fullWidth style={{background: "#F5F5F5"}}/>
-              </FormControl>
-            </div>
-
-            <div>
-              <label
-                for="Software Tool"
+                htmlFor="Software Tool"
                 style={{
                   marginTop: "20px",
                   marginLeft: "9px",
@@ -86,13 +112,18 @@ export const PostTask = () => {
                 Software Tool
               </label>
               <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
-                <TextField id="outlined-error" variant="outlined" fullWidth style={{background: "#F5F5F5"}} />
+                <TextField
+                  id="outlined-error"
+                  variant="outlined"
+                  fullWidth
+                  style={{ background: "#F5F5F5" }}
+                />
               </FormControl>
             </div>
 
             <div>
               <label
-                for="Category"
+                htmlFor="Category"
                 style={{
                   marginTop: "20px",
                   marginLeft: "9px",
@@ -102,7 +133,12 @@ export const PostTask = () => {
                 Category
               </label>
               <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
-                <TextField id="outlined-error" variant="outlined" fullWidth style={{background: "#F5F5F5"}} />
+                <TextField
+                  id="outlined-error"
+                  variant="outlined"
+                  fullWidth
+                  style={{ background: "#F5F5F5" }}
+                />
               </FormControl>
             </div>
           </Form>
--- src/ui/Layout.js
+++ src/ui/Layout.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "./layout.css";
 const Layout = ({ children }) => {
+  const userData = useSelector((state) => state.authReducer.userData);
+  
+  if (!userData?.token) {
+    return children;
+  }
+
   return (
     <div className="layout">
       <Navbar />
